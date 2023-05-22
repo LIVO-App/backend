1b27ae40ab270c345a407a2f7c8132c4362ecc27
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-05-22 10:10:05 +0200

    Migrated courses / api to version v2

--- routes/apiV2.js
+++ routes/apiV2.js
@@ -4,8 +4,10 @@ const express = require('express');
 const apiV2 = express.Router();
 
 const studentRouter = require('./apiV2/student');
+const courseRouter = require('./apiV2/course');
 //const teacherRouter = require('./apiV1/teacher');
 
 apiV2.use('/students',studentRouter);
+apiV2.use('/courses', courseRouter);
 
 module.exports = apiV2;

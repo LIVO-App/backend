1f08447a9d73e895466ff8df624e99134ed7519b
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-06-13 16:51:03 +0200

    Fixed project_class_components visualization with ordinary class information and changed check on teacher_id

--- controllers/projectClassController.js
+++ controllers/projectClassController.js
@@ -13,6 +13,7 @@ process.env.TZ = 'Etc/Universal';
 
 module.exports.get_project_class_components = async (req, res) => {
     let teacher_id = req.query.teacher_id;
+    let query = teacher_id ? {teacher_id: teacher_id} : {}; 
     if(req.loggedUser.role == "teacher"){
         if(teacher_id!=undefined){
             if(req.loggedUser._id != teacher_id){
@@ -20,6 +21,8 @@ module.exports.get_project_class_components = async (req, res) => {
                 console.log('project_class components: unauthorized access');
                 return;
             }
+        } else {
+            teacher_id = req.loggedUser._id;
         }
     } else {
         res.status(401).json({status: "error", description: MSG.notAuthorized});
@@ -29,7 +32,9 @@ module.exports.get_project_class_components = async (req, res) => {
     let course_id = req.params.course;
     let block_id = req.params.block;
     let section = req.query.section;
+    query["section"] = section;
     let associated_class = req.query.assoc_class;
+    query["assoc_class"] = associated_class
     let cmps = await projectClassesSchema.classComponents(course_id, block_id, section, teacher_id, associated_class);
     if (!cmps) {
         res.status(400).json({status: "error", description: MSG.missingParameter});
@@ -40,13 +45,16 @@ module.exports.get_project_class_components = async (req, res) => {
         return {
             id: cmp.id,
             name: cmp.name,
-            surname: cmp.surname
+            surname: cmp.surname,
+            ord_class_study_year: cmp.ordinary_class_study_year,
+            ord_class_address: cmp.ordinary_class_address,
+            ord_class_section: cmp.section
         }
     });
     let response = {
         path: "/api/v1/project_classes/:course/:block/components",
         single: false,
-        query: {section: section, teacher_id: teacher_id, assoc_class: associated_class},
+        query: query,
         date: new Date(),
         data: data_cmps
     };
--- livoapp_db.sql
+++ livoapp_db.sql
@@ -471,7 +471,7 @@ INSERT INTO `learning_area` (`id`, `italian_title`, `english_title`, `italian_de
 ('COM', 'Area della comunicazione', 'Communication area', NULL, NULL),
 ('SGET', 'Area storico-giuridico-economica e del territorio', 'Historical-legal-economic and territorial area', NULL, NULL),
 ('SM', 'Area scientifico-matematica', 'Scientific-mathematical area', NULL, NULL),
-('TEC', 'Area tecnologica', 'Technology area', 'In quest\'area, uno studente ha la possibilità di avere a che fare con le tecnologie all\'avanguardia nel settore', 'In this area, a student has the opportunity to deal with cutting-edge technologies in the field'),
+('TEC', 'Area tecnologica, artistico, motoria', 'Technology area', 'In quest\'area, uno studente ha la possibilità di avere a che fare con le tecnologie all\'avanguardia nel settore', 'In this area, a student has the opportunity to deal with cutting-edge technologies in the field'),
 ('TRA', 'Area trasversale', 'Trasversal area', 'Scienze motorie e sportive; religione cattolica; attività alternative', 'Exercise science and sports; Catholic religion; alternative activities');
 
 -- --------------------------------------------------------

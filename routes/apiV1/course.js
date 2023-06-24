46d45a0a3647d9685ac5a8f6efc366273950163a
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-06-24 23:32:47 +0200

    Removed learning context from course api and updated opento api to fit new database structure

--- routes/apiV1/course.js
+++ routes/apiV1/course.js
@@ -12,6 +12,5 @@ router.get('/', coursesHandler.get_courses);
 router.get('/:course_id', coursesHandler.get_course);
 router.get('/:course_id/opento', opentoHandler.get_institute_classes);
 router.get('/:course_id/teachings', courseteachingHandler.get_teachings);
-router.get('/:course_id/learning_contexts', coursecontexHandler.get_contexts);
 
 module.exports = router;

f5e5a8281fce91c5d607c96effc6e607e03b9dbb
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-07-21 20:29:04 +0200

    Fixed bug in get_models method

--- models/coursesModel.js
+++ models/coursesModel.js
@@ -175,14 +175,22 @@ module.exports = {
     async get_models(teacher_id, only_recent = true, not_confirmed = true){
         try {
             conn = await pool.getConnection()
-            let sql = `SELECT c.id, CASE WHEN pc.italian_displayed_name IS NULL THEN c.italian_title ELSE pc.italian_displayed_name END AS 'italian_title', CASE WHEN pc.english_displayed_name IS NULL THEN c.english_title ELSE pc.english_displayed_name END AS 'english_title', c.creation_date FROM course AS c`
+            let sql = `SELECT c.id`
+            if(only_recent){
+                sql += `, c.italian_title, c.english_title`
+            } else {
+                sql += `, CASE WHEN pc.italian_displayed_name IS NULL THEN c.italian_title ELSE pc.italian_displayed_name END AS 'italian_title', CASE WHEN pc.english_displayed_name IS NULL THEN c.english_title ELSE pc.english_displayed_name END AS 'english_title'`
+            }
+            sql += `, c.creation_date FROM course AS c`
+            if(!only_recent){
+                sql += ` LEFT JOIN project_class AS pc ON pc.course_id = c.id`
+            }
             if(only_recent){ // I want to have the last 3 models available
                 sql += `WHERE c.admin_confirmation IS NOT NULL and c.certifying_admin_id IS NOT NULL`
                 if(teacher_id != undefined){
                     sql += ` AND c.proposer_teacher_id = ${teacher_id}`
                 }
             } else { // I want to check the propositions of the courses. not_confirmed tells if the user wants to see only the ones not confirmed yet.
-                sql += ` LEFT JOIN project_class AS pc ON pc.course_id = c.id`
                 if(teacher_id!=undefined && not_confirmed){
                     sql += ` WHERE c.proposer_teacher_id = ${teacher_id} AND ((c.admin_confirmation IS NULL AND c.certifying_admin_id IS NOT NULL) OR (pc.admin_confirmation IS NULL and pc.certifying_admin_id IS NULL))`
                 } else if (teacher_id!=undefined){

7e10039513b4fab47ef8954d24e350f3d75958db
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-09-07 11:54:56 +0200

    Fixed grade insertion to avoid insertion in future grades

--- controllers/gradesController.js
+++ controllers/gradesController.js
@@ -4,6 +4,7 @@ const gradesSchema = require('../models/gradesModel');
 const project_classSchema = require('../models/projectClassModel');
 const studentModel = require('../models/studentModel');
 const teacherModel = require('../models/teacherModel');
+const learningSessionModel = require('../models/learning_sessionsModel')
 
 let MSG = {
     notFound: "Resource not found",
@@ -101,6 +102,17 @@ module.exports.insert_grade = async (req, res) => {
     let teacher_id = req.query.teacher_id;
     let course_id = req.query.course_id;
     let session_id = req.query.session_id;
+    let session_exist = await learningSessionModel.read(session_id)
+    if(!session_exist){
+        res.status(404).json({status: "error", description: MSG.notFound});
+        console.log('class grades: session not found');
+        return;
+    }
+    if(new Date(session_exist.start) > new Date()){
+        res.status(400).json({status: "error", description: "Tried to add a grade for a future class"});
+        console.log('class grades: future block');
+        return;
+    }
     let teacher_exists = await teacherModel.read_id(teacher_id)
     if(!teacher_exists){
         res.status(401).json({status: "error", description: MSG.notAuthorized});
